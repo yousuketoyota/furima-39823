5955c2333da153e560c1b3d0daca7f7bebd14025
yousuketoyota <147295357+yousuketoyota@users.noreply.github.com>
2024-01-04 23:53:50 +0900

    テストコードの実装

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe User, type: :model do
 
   describe 'ユーザー新規登録' do
     context '新規登録できる場合' do
-      it 'すべての項目の入力が存在する場合登録できる' do
+      it 'nicknameとemail、passwordとpassword_confirmationが存在すれば登録できる' do
         expect(@user).to be_valid
       end
     end
