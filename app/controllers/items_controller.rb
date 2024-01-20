a8212092a0a738e3434e3bc74580bd8e62f92256
yousuketoyota <147295357+yousuketoyota@users.noreply.github.com>
2024-01-20 17:47:04 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-before_action :authenticate_user!, only: [:new, :create]
-before_action :set_item, only: [:show]
+before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+before_action :set_item, only: [:show, :edit, :update]
   
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -15,21 +15,37 @@ before_action :set_item, only: [:show]
     if @item.save
       redirect_to root_path
     else
-      render :new,  status: :unprocessable_entity
+      render :new, status: :unprocessable_entity
     end
   end
 
   def show
+
   end
+
+  def edit
+    unless @item.user == current_user
+      redirect_to root_path
+    end  
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
 
   private
 
-  def item_params
-    params.require(:item).permit(:title, :explanation, :category_id, :condition_id, :shipping_fee_id, :shipping_source_id, :shipping_schedule_id, :price, :image).merge(user_id: current_user.id)
-  end
-
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def item_params
+    params.require(:item).permit(:title, :explanation, :category_id, :condition_id, :shipping_fee_id, :shipping_source_id, :shipping_schedule_id, :price, :image).merge(user_id: current_user.id)
   end
 
 end  
