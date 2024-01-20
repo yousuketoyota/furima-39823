6b1d8547de2a3a48793feaf3842864c4e51aa465
yousuketoyota <147295357+yousuketoyota@users.noreply.github.com>
2024-01-20 20:06:08 +0900

    Revert "revert"
    This reverts commit 6e6ac7f6dc126e16d83bc39a2c60660d84e5555f.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
-  
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
