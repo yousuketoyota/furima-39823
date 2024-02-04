cb0d85a62c675d3690aa321609a870d41847ca7d
yousuketoyota <147295357+yousuketoyota@users.noreply.github.com>
2024-02-04 10:31:02 +0900

    修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,25 +1,10 @@
-window.addEventListener('turbo:load', () => {
-
+window.addEventListener('load', () => {
   const priceInput = document.getElementById("item-price");
-  
-  const updatePrice = () => {
-    const price = parseFloat(priceInput.value);
-    const addTaxDom = document.getElementById("add-tax-price");
-    const tax = Math.floor(price * 0.1);
-    addTaxDom.innerHTML = tax;
-    
-    const addProfitDom = document.getElementById("profit");
-    const profit = Math.floor(price - tax);
-    addProfitDom.innerHTML = profit;
-  };
-  
-  updatePrice();
-  
-  const resetEventListeners = () => {
-    priceInput.removeEventListener("input", updatePrice); // 既存のリスナーを削除
-    priceInput.addEventListener("input", updatePrice); // 新しいリスナーを追加
-  };
-
-  priceInput.addEventListener("input", resetEventListeners);
-});
-
+  priceInput.addEventListener("input", () => {
+  const inputValue = priceInput.value;
+  const addTaxDom = document.getElementById("add-tax-price");
+  const ProfitDom = document.getElementById("profit");
+  addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
+  ProfitDom.innerHTML = inputValue - addTaxDom.innerHTML;
+  })
+})
