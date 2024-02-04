1d898effa38907921f9f8d8963829954c3fdb7d9
yousuketoyota <147295357+yousuketoyota@users.noreply.github.com>
2024-02-04 11:12:40 +0900

    修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -3,7 +3,7 @@ window.addEventListener('turbo:load', () => {
   const addTaxDom = document.getElementById("add-tax-price");
   const proFitDom = document.getElementById("profit");
   
-  const recalculateAndDisplay = () => {
+  const priceInputHandler = () => {
     const inputValue = parseFloat(priceInput.value);
     const tax = Math.floor(inputValue * 0.1);
     addTaxDom.innerHTML = tax;
@@ -11,8 +11,8 @@ window.addEventListener('turbo:load', () => {
   };
   
   // 初回の計算
-  recalculateAndDisplay();
+  priceInputHandler();
   
   // 再度価格が入力された場合に再計算する
-  priceInput.addEventListener("input", recalculateAndDisplay);
+  priceInput.addEventListener("input", priceInputHandler);
 });
