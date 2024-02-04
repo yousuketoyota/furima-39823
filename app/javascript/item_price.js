d94db9c0e82a19d60d6b06699dd8de212f1e2d31
yousuketoyota <147295357+yousuketoyota@users.noreply.github.com>
2024-02-04 10:27:12 +0900

    修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,12 +1,25 @@
 window.addEventListener('turbo:load', () => {
+
   const priceInput = document.getElementById("item-price");
-  priceInput.addEventListener("input", () => {
+  
+  const updatePrice = () => {
     const price = parseFloat(priceInput.value);
     const addTaxDom = document.getElementById("add-tax-price");
     const tax = Math.floor(price * 0.1);
     addTaxDom.innerHTML = tax;
+    
     const addProfitDom = document.getElementById("profit");
     const profit = Math.floor(price - tax);
     addProfitDom.innerHTML = profit;
-  });
+  };
+  
+  updatePrice();
+  
+  const resetEventListeners = () => {
+    priceInput.removeEventListener("input", updatePrice); // 既存のリスナーを削除
+    priceInput.addEventListener("input", updatePrice); // 新しいリスナーを追加
+  };
+
+  priceInput.addEventListener("input", resetEventListeners);
 });
+
