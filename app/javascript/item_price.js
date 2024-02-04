53ecd26122d325267fff6e0920b10e3058c97e4b
yousuketoyota <147295357+yousuketoyota@users.noreply.github.com>
2024-02-04 10:36:18 +0900

    修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,10 +1,11 @@
-window.addEventListener('load', () => {
+window.addEventListener('turbo:load', () => {
   const priceInput = document.getElementById("item-price");
-  priceInput.addEventListener("input", () => {
-  const inputValue = priceInput.value;
   const addTaxDom = document.getElementById("add-tax-price");
-  const ProfitDom = document.getElementById("profit");
-  addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
-  ProfitDom.innerHTML = inputValue - addTaxDom.innerHTML;
+  const totalInput = document.getElementById("profit");
+  const tax = 0.1
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value
+    addTaxDom.innerHTML = Math.floor(inputValue * tax)
+    totalInput.innerHTML = Math.floor(inputValue - addTaxDom.innerHTML )
   })
-})
+});
