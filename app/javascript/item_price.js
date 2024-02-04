ed92c76b99a140b50705600cb567290f35e739ac
yousuketoyota <147295357+yousuketoyota@users.noreply.github.com>
2024-02-04 10:59:35 +0900

    修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,21 +1,10 @@
 window.addEventListener('turbo:load', () => {
   const priceInput = document.getElementById("item-price");
-  const addTaxDom = document.getElementById("add-tax-price");
-  const totalInput = document.getElementById("profit");
-  const tax = 0.1;
-
-  const calculateAndDisplay = () => {
-    const inputValue = parseFloat(priceInput.value);
-
-    if (!isNaN(inputValue)) { // 入力が数字であることを確認
-      const taxAmount = Math.floor(inputValue * tax);
-      addTaxDom.innerHTML = taxAmount;
-      totalInput.innerHTML = Math.floor(inputValue - taxAmount);
-    } else { // 入力が数字でない場合、手数料と利益を空にする
-      addTaxDom.innerHTML = "";
-      totalInput.innerHTML = "";
-    }
-  };
-
-  priceInput.addEventListener("input", calculateAndDisplay);
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = Math.floor(inputValue * 0.1)
+    const proFitDom = document.getElementById("profit");
+    proFitDom.innerHTML =inputValue - parseInt(addTaxDom.innerHTML);
+  });
 });
